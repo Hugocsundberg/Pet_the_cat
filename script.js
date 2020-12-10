c7ee99d6c6920865cb9247d7580df7e4dcd286e2
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2020-12-10 14:20:29 +0100

    Update

--- script.js
+++
@@ -1,8 +0,0 @@
-const catContainer = document.querySelector('.catContainer')
-const mobileLog = document.querySelector('.mobileLog')
-
-const eventCount = 0
-catContainer.addEventListener('touchmove', () => {
-    eventCount++
-    mobileLog.textContent = eventCount;
-})
